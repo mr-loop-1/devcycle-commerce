169671835570db83578db0c7802e6731dd66d86d
mr-loop-1 <loopolecreator1@gmail.com>
2024-12-03 15:08:48 +0530

    add: flags sdk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './pages/home'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  useIsDevCycleInitialized,
+  withDevCycleProvider,
+} from '@devcycle/react-client-sdk';
+
+import './App.css';
+import HomePage from './pages/home';
+import { LoadingSpinner } from '@/components/Spinner';
 
 function App() {
+  let devCycleReady = false;
+  try {
+    devCycleReady = useIsDevCycleInitialized();
+  } catch (err) {
+    console.error(err);
+    return <LoadingSpinner />;
+  }
+
+  if (!devCycleReady) return <LoadingSpinner />;
+
   return (
     <Router>
       <Routes>
-        <Route path='' Component={HomePage}/>
+        <Route path="" Component={HomePage} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default withDevCycleProvider({
+  sdkKey: import.meta.env.VITE_SDK_KEY,
+})(App);
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,74 @@
-import * as react from 'react';
+import react, { useState } from 'react';
+import { useVariableValue } from '@devcycle/react-client-sdk';
 
 export default function HomePage() {
-  return <div>
-    
-  </div>
+  const saleActiveFlag = useVariableValue('sale-active', false);
+
+  return (
+    <div>
+      {/* Hello there !{saleActiveFlag ? <img src="/sale/banner.jpg" /> : 'No sale'} */}
+      <Navbar />
+      <div className="content">
+        <div className="poster-area">
+          <div className="poster-data">
+            <p className="poster-head">Free Delivery!</p>
+            <p className="poster-desc">
+              Don't miss it out! Only today, get free{' '}
+              <b style={{ fontSize: '22px' }}>Next Day</b> delivery on all your
+              orders.
+            </p>
+          </div>
+          <button className="browse-btn">Browse products</button>
+        </div>
+        <img src={Delivery} className="delivery" />
+        <Popular />
+      </div>
+    </div>
+  );
+}
+
+import { React, useState } from 'react';
+import './home.css';
+import Delivery from '../imgs/delivery.png';
+import Popular from './Category/Popular';
+import Navbar from './Navbar';
+
+function Home() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  document.title = 'Amazon';
+
+  const handleScroll = () => {
+    window.scrollTo({
+      top: scrollPosition + 750,
+      behavior: 'smooth',
+    });
+    setScrollPosition(scrollPosition + 750);
+    setTimeout(() => {
+      setScrollPosition(0);
+    }, 100);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="content">
+        <div className="poster-area">
+          <div className="poster-data">
+            <p className="poster-head">Free Delivery!</p>
+            <p className="poster-desc">
+              Don't miss it out! Only today, get free{' '}
+              <b style={{ fontSize: '22px' }}>Next Day</b> delivery on all your
+              orders.
+            </p>
+          </div>
+          <button onClick={handleScroll} className="browse-btn">
+            Browse products
+          </button>
+        </div>
+        <img src={Delivery} className="delivery" />
+        <Popular />
+      </div>
+    </>
+  );
 }
