ffce20aa311149d3ee7a2cbc0a48bd3ea5adcf82
mr-loop-1 <loopolecreator1@gmail.com>
2024-12-20 18:03:31 +0530

    add: add and remove cart buttons

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,14 +1,28 @@
+import { Button } from './ui/button';
 import { Card } from './ui/card';
 
 export default function Products({ category }) {
+  const cart = [2];
   return (
     <div>
       <div className="">{category.title}</div>
       <div className="w-full flex overflow-x-auto">
         {category.products.map((product) => {
           return (
-            <Card className="p-2 w-60 h-60 flex-shrink-0">
+            <Card className="p-2 w-60 h-fit flex-shrink-0">
               <img src={`/products/${product.slug}.png`} />
+              <div className="flex w-full justify-between">
+                <div className="">weqda</div>
+                {cart.includes(product.id) ? (
+                  <Button className="bg-red-400 hover:bg-orange-500">
+                    <img className="w-6 h-6" src="/remove.svg" />
+                  </Button>
+                ) : (
+                  <Button className="bg-lime-300 hover:bg-lime-500">
+                    <img className="w-6 h-6" src="/add.svg" />
+                  </Button>
+                )}
+              </div>
             </Card>
           );
         })}
