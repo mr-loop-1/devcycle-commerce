b13470739f672df68d1c1eb3306c2fd0743ec3f7
mr-loop-1 <loopolecreator1@gmail.com>
2024-12-20 12:07:26 +0530

    add: showcase layout

--- frontend/src/components/Showcase.jsx
+++ frontend/src/components/Showcase.jsx
@@ -4,12 +4,13 @@ import { Card } from './ui/card';
 
 export default function Showcase() {
   return (
-    <div id="showcase" className="mx-20">
-      <div className="flex justify-around">
-        <div className="grow-[3] bg-gray-400">
-          <Card className="w-full h-full"></Card>
+    <div id="showcase" className="mx-4 lg:mx-20">
+      <div className="flex flex-col md:flex-row justify-around w-full h-96 md:h-96">
+        <div className="w-full md:w-[66%] h-[66%] md:h-full bg-gray-400 p-2"></div>
+        <div className="w-full md:w-[33%] h-[33%] md:h-full bg-green-200 p-2 flex flex-row md:flex-col justify-between">
+          <div className="w-full h-full bg-red-100"></div>
+          <div className="w-full h-full bg-blue-100"></div>
         </div>
-        <div className="grow bg-green-200">wqda</div>
       </div>
     </div>
   );
