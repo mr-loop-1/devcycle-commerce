54056030210b9bab917e6691556b37aa5d3b60a5
mr-loop-1 <loopolecreator1@gmail.com>
2024-12-21 16:03:55 +0530

    add: product and category data structure

---
+++ frontend/api/db/config.js
@@ -0,0 +1,16 @@
+export const shippingType = {
+  1: 'Free',
+  2: 'Primary',
+  3: '3rd Party',
+};
+
+export const country = {
+  IN: {
+    name: 'India',
+    currency: 'Rs',
+  },
+  CA: {
+    name: 'Canada',
+    currency: 'Dollar',
+  },
+};
---
+++ frontend/api/db/products.js
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    categoryId: 1,
+    title: "Men's Shirt",
+    slug: 1,
+    available: ['IN', 'CA'],
+    specs: {
+      IN: {
+        price: 199.0,
+        discount: 25.25,
+        salePrice: 174.75,
+        stock: 943,
+        shippingType: 3,
+        cost: 23,
+      },
+      CA: {
+        price: 199.0,
+        discount: 25.25,
+        salePrice: 174.75,
+        stock: 943,
+        shippingType: 3,
+        cost: 23,
+      },
+    },
+  },
+];
