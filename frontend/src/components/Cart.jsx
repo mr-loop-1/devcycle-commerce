f4958a5fe93211c63fc66b73a2de9dcfa92b5ad4
mr-loop-1 <loopolecreator1@gmail.com>
2024-12-22 21:05:27 +0530

    add: cart decoration

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -10,7 +10,6 @@ import {
   SheetClose,
 } from '@/components/ui/sheet';
 import { Card } from './ui/card';
-import { useVariableValue } from '@devcycle/react-client-sdk';
 import CartProduct from './CartProducts';
 import { useCart } from '@/contexts/CartProvider';
 import { getCartValue } from '../../api/api';
@@ -26,22 +25,56 @@ export default function CartDrawer({ cart, country, isSale, shippingWaiver }) {
   });
 
   return (
-    <Sheet>
-      <SheetTrigger asChild>
+    <Sheet className="w-96">
+      <SheetTrigger aria-describedby="kjsdna" asChild>
         <Button variant="outline">Open</Button>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="min-w-[90%] md:min-w-[25%]">
         <SheetHeader>
-          <SheetTitle>My Cart</SheetTitle>
+          <SheetTitle>My Cart ({cart.length})</SheetTitle>
         </SheetHeader>
 
         <div className="flex flex-col h-full justify-between">
-          <div id="cart-items" className=""></div>
-          <Card id="cart-price-and-coupons" className="mb-2">
-            <div className="">
-              {cartData.productsData.map((product) => {
-                return <CartProduct product={product} dispatch={dispatch} />;
-              })}
+          <div className="flex flex-col overflow-y-auto h-[80%] my-4 py-4 shadow-inner no-scrollbar">
+            {cartData.productsData.map((product) => {
+              return (
+                <CartProduct
+                  product={product}
+                  shippingWaiver={shippingWaiver}
+                  dispatch={dispatch}
+                />
+              );
+            })}
+          </div>
+          <div className="p-2 bg-lime-300 font-mono rounded-xl">
+            Congratulations, you have saved {cartData.priceData.discount} on
+            this order
+          </div>
+          <Card className="mt-2 mb-6 md:mb-10">
+            <div
+              id="cart-drawer-cost"
+              className="flex flex-col font-mono  mx-2"
+            >
+              <span className="flex justify-between">
+                <span>Total MRP:</span>
+                <span>{cartData.priceData.mrp}</span>
+              </span>
+              <span className="flex justify-between">
+                <span className="">Sale Discount:</span>
+                <span>{cartData.priceData.mrp}</span>
+              </span>
+              <span className="flex justify-between">
+                <span>Shipping Charges:</span>
+                <span>{cartData.priceData.mrp}</span>
+              </span>
+              <span className="flex justify-between">
+                <span>Shipping Discount: </span>
+                <span>{cartData.priceData.mrp}</span>
+              </span>
+              <span className="flex justify-between text-2xl font-semibold">
+                <span className="">Total Cost:</span>
+                <span>{cartData.priceData.mrp}</span>
+              </span>
             </div>
           </Card>
         </div>
--- frontend/src/components/CartProducts.jsx
+++ frontend/src/components/CartProducts.jsx
@@ -2,23 +2,40 @@ import Product from './Product.jsx';
 import { Button } from './ui/button.jsx';
 import { Card } from './ui/card.jsx';
 
-export default function CartProduct({ product, dispatch }) {
+export default function CartProduct({ product, shippingWaiver, dispatch }) {
   return (
-    <Card className="p-2 w-60 h-fit flex-shrink-0">
-      <img src={`/products/${product.slug}.png`} />
-      <div className="flex w-full justify-between">
-        <div className="">weqda</div>
-        <Button
-          className="bg-red-500 hover:bg-red-700"
-          onClick={() =>
-            dispatch({
-              type: 'remove',
-              id: product.id,
-            })
-          }
-        >
-          <img className="w-6 h-6" src="/remove.svg" />
-        </Button>
+    <Card className="p-2 mb-2 flex flex-col">
+      <div className="flex justify-between">
+        <div className="h-28 md:h-36 aspect-square ">
+          <img
+            className="w-full h-full object-contain rounded-2xl "
+            src={`/products/${product.slug}.png`}
+          />
+        </div>
+        <div className="ml-2 max-w-full flex flex-col break-words text-wrap">
+          <span className="">{product.title}</span>
+          <span className="break-words">
+            123 <span className="ml-1 line-through">312</span>
+          </span>
+          <span className="">
+            {product.specs.shippingType == 1 || shippingWaiver == 'high' ? (
+              <span>Free Shipping</span>
+            ) : (
+              <span>+{product.specs.cost}</span>
+            )}
+          </span>
+        </div>
+      </div>
+      <div
+        onClick={() =>
+          dispatch({
+            type: 'remove',
+            id: product.id,
+          })
+        }
+        className="text-red-700 cursor-pointer w-full mt-2 hover:underline text-center"
+      >
+        Remove
       </div>
     </Card>
   );
